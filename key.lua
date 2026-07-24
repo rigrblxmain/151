2e98c48f37f003e7734b2a5d0da883b4b7732886
rigrblxmain <666platy666@gmail.com>
2026-07-24 13:03:09 +0800

    Update key.lua

--- key.lua
+++ key.lua
@@ -1,6 +1,3 @@
--- This is the code you OBFUSCATE and put on Pastebin/GitHub
--- It secretly checks the global variable your main script sets.
-
 if getgenv().AltKey == "I don't know the key" then
     getgenv().EG_PremiumUnlocked = true
 else
